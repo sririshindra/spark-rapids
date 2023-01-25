811902a7a79ab57f059971943ecbae23e5567c5f
rishi <sririshindra@gmail.com>
2023-01-25 13:23:32 -0800

    Disable HiveTextReaders for CDH

--- sql-plugin/src/main/330cdh/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
+++ sql-plugin/src/main/330cdh/scala/com/nvidia/spark/rapids/shims/SparkShims.scala
@@ -17,8 +17,28 @@
 package com.nvidia.spark.rapids.shims
 
 import com.nvidia.spark.rapids._
+import org.apache.spark.sql.execution.SparkPlan
+//import org.apache.spark.sql.hive.execution.HiveTableScanExec
 
 import org.apache.spark.sql.execution.command.{CreateDataSourceTableAsSelectCommand, DataWritingCommand, RunnableCommand}
+
+//object HiveOverrides {
+//
+//  def getHiveTableExecRule() = {
+//    GpuOverrides.exec[HiveTableScanExec](
+//      desc = "CDH-specific override to disable HiveTableScanExec, " +
+//        "to intercept Hive delimited text table reads.",
+//      ExecChecks(TypeSig.commonCudfTypes + TypeSig.DECIMAL_128,
+//        TypeSig.all),
+//      (p, conf, parent, r) => new SparkPlanMeta[HiveTableScanExec](p, conf, parent, r) {
+//        override def convertToGpu(): GpuExec = null // No substitutions.
+//        override def tagPlanForGpu(): Unit = {
+//          willNotWorkOnGpu("No soup for you! HiveTableScanExec disabled on CDH.")
+//        }
+//      }
+//    )
+//  }
+//}
 
 object SparkShimImpl extends Spark330PlusNonDBShims with AnsiCastRuleShims {
   override def getDataWriteCmds: Map[Class[_ <: DataWritingCommand],
@@ -33,4 +53,11 @@ object SparkShimImpl extends Spark330PlusNonDBShims with AnsiCastRuleShims {
       RunnableCommandRule[_ <: RunnableCommand]] = {
     Map.empty
   }
+
+  override def getExecs: Map[Class[_ <: SparkPlan], ExecRule[_ <: SparkPlan]] = {
+
+    super.getExecs ++ Seq(org.apache.spark.sql.hive.cdh.HiveOverrides.getHiveTableExecRule())
+      .map(r => (r.getClassFor.asSubclass(classOf[SparkPlan]), r)).toMap
+  }
+
 }
